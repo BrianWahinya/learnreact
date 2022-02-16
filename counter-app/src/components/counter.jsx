2d0931af41b659aead37ddb1b17013f9f4ced280
bngure <brianwahinyangure@gmail.com>
2022-02-16 04:09:55 +0300

    Added add and minus products and synchronized product value and total products value

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -17,8 +17,10 @@ class Counter extends React.Component {
             street: 'K street',
             city: 'Nairobi'
         },
-        tags: ['Phone', 'Torch', 'Laptop', 'Mouse', 'Screen']
+        tags: ['Phone', 'Torch', 'Laptop', 'Mouse', 'Screen'],
+        tagsObj:{}
         // countName: method - getBadgeClasses
+        // tagObj: object containing tags and their respective count
     }
 
     render() {
@@ -28,16 +30,27 @@ class Counter extends React.Component {
                 {/* <img src={this.state.imageUrl} alt='react'/> */}
                 {/* <h1>First Component</h1> */}
                 <span className={this.getBadgeClasses()}>{this.state.countName}</span>
-                <button onClick={() => this.countIncrement({id:2})} className='btn btn-success btn-sm'>Increment</button>
+                {/* <button onClick={() => this.countIncrement({id:2})} className='btn btn-success btn-sm'>Increment</button> */}
                 {this.renderTags()}
             </>
         );
     }
 
     renderTags() {
-        const {tags} = this.state;
+        const {tags, tagsObj} = this.state;
         if (tags.length === 0) return <p>No tags available</p>;
-        return <ul>{tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+        
+        return <ul>
+            { tags.map(tag => {
+                tagsObj[tag] = tagsObj[tag] || 0; 
+                return <li key={tag}>
+                    <span className='badge bg-dark'>{tagsObj[tag]}</span>
+                    {tag /*Tag Label */}
+                    {<button id={tag} onClick={() => this.countProducts('add', tag)} className='badge m-2 btnAdd bg-primary'>+</button> /*Add button*/}
+                    {<button id={tag} onClick={() => this.countProducts('minus', tag)} className='badge m-2 btnSubtract bg-danger'>-</button> /*Subtract button*/}
+                </li>
+            })}
+        </ul>;
     }
 
     getBadgeClasses() {
@@ -50,6 +63,7 @@ class Counter extends React.Component {
             classes += 'primary';
             this.state.countName = this.state.count;
         }
+        console.log(this.state);
         return classes;
     }
 
@@ -59,11 +73,29 @@ class Counter extends React.Component {
     // }
 
     // use fat-arrow function instead of using constructor
-    countIncrement = productObj => {
-        // this.state.count++;
-        console.log("product object", productObj);
-        this.state.count++;
-        this.setState({ count: this.state.count });
+    countProducts = (instruction, productId) => {
+        let {count, tagsObj} = this.state;
+        // console.log('Before tagsObj', tagsObj)
+        let productAmount = tagsObj[productId];
+        switch(instruction){
+            case 'add':
+                count++;
+                productAmount++;         
+                break;
+            case 'minus':
+                if(count !== 0 && productAmount !== 0){
+                    count--;
+                    productAmount--;
+                }                
+                break;                
+        }
+        tagsObj[productId] = productAmount;
+        // console.log('After tagsObj', tagsObj);
+        this.setState({
+            count: count,
+            tagsObj: tagsObj
+        })
+        // console.log('current state', this.state)       
     }
 
     // pass arguments to countIncrement
