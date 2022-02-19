99ba8fe4e7446d0f0a6710f7f053f90adfd5def9
bngure <brianwahinyangure@gmail.com>
2022-02-19 15:47:19 +0300

    Counter-app NavBar component added stateless functional component example

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 
+// Method 1
+// Class
 class NavBar extends React.Component {
   render(){
     return (
@@ -10,7 +12,20 @@ class NavBar extends React.Component {
       </nav>
     )
   }
-
 }
+
+/*
+// Method 2
+// Stateless Functional Components
+const NavBar = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Total: {props.totalProducts()}</a>
+      </div>
+    </nav>
+  )
+}
+*/
 
 export default NavBar;
