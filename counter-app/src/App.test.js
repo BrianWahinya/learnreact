d1b07f86583094ecfac6099d5aa2bdaf5016a5a8
Brian Wahinya <44973562+BrianWahinya@users.noreply.github.com>
2022-02-22 13:32:15 +0300

    Update App.test.js

--- counter-app/src/App.test.js
+++ counter-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const navbarTitle = screen.getByText(/wekaweka/i);
+  const navbarTitle = screen.getByText(/Total/i);
   expect(navbarTitle).toBeInTheDocument();
 });
